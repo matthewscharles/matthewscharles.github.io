947882f5869ee441381647810974741c3c861f7b
Charles Matthews <charles0matthews@gmail.com>
2025-01-17 13:23:51 -0500

    Update main.js

--- site/src/main.js
+++ site/src/main.js
@@ -5,6 +5,8 @@ document.querySelector('#app').innerHTML = `
 <h1>Charles Matthews</h1>
 <h2>Technical Audio and Music Designer</h2>
     ${logos}
+    <h3>Game audio demo playlist</h3>
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/videoseries?si=2lW76TEhJVdbHEOR&amp;list=PLIKWa1FaZD5y24pnfeUiXkJ6GzWY5KAUE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
     <h3>Unreal Engine plugin development</h3>
 <a href="https://github.com/matthewscharles/metasound-branches">Branches: Metasound Plugins</a>
 <div class="embed-container">
