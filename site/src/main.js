5c776bd126f6acda0716bf5c88ad67d7502e2077
Charles Matthews <charles0matthews@gmail.com>
2025-01-19 17:27:17 -0500

    General update

--- site/src/main.js
+++ site/src/main.js
@@ -1,17 +1,34 @@
 import './style.css'
 import logos from './logos.html?raw'
+import links from './links.html?raw'
 
 document.querySelector('#app').innerHTML = `
-<h1>Charles Matthews</h1>
-<h2>Technical Audio and Music Designer</h2>
-    ${logos}
+
+    <div class="card logos">
+      ${logos}
+    </div>
+    <h3>Project links</h3>
+    <div class="card links">
+      ${links}
+    </div>
+    <hr>
+    <div class="card">
     <h3>Game audio demo playlist</h3>
-    <iframe width="560" height="315" src="https://www.youtube.com/embed/videoseries?si=2lW76TEhJVdbHEOR&amp;list=PLIKWa1FaZD5y24pnfeUiXkJ6GzWY5KAUE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-    <h3>Unreal Engine plugin development</h3>
-<a href="https://github.com/matthewscharles/metasound-branches">Branches: Metasound Plugins</a>
-<div class="embed-container">
-    <iframe width="640" height="390" 
-    src="https://www.youtube.com/embed/HgudP5fzDWM" 
-    frameborder="0" allowfullscreen></iframe>
+      <div class="embed-container">
+          <iframe width="640" height="390" 
+          src="https://www.youtube.com/embed/videoseries?si=2lW76TEhJVdbHEOR&amp;list=PLIKWa1FaZD5y24pnfeUiXkJ6GzWY5KAUE" 
+          frameborder="0" allowfullscreen></iframe>
+      </div>
+    </div>
+    <hr>
+    <div class="card">
+      <h3>Unreal Engine plugin development</h3>
+      <a href="https://github.com/matthewscharles/metasound-branches">Branches: Metasound Plugins</a>
+      <div class="embed-container">
+          <iframe width="640" height="390" 
+          src="https://www.youtube.com/embed/HgudP5fzDWM" 
+          frameborder="0" allowfullscreen></iframe>
+      </div>
+    </div>
 </div>
 `
