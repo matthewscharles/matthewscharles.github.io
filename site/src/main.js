e3a3cdab1e6955374966d38679ccfb90d4b87077
Charles Matthews <charles0matthews@gmail.com>
2025-01-23 12:28:58 -0500

    update projects

--- site/src/main.js
+++ site/src/main.js
@@ -4,9 +4,10 @@ import links from './projects.html?raw';
 import playlist from './playlist.html?raw';
 
 document.querySelector('#app').innerHTML = `
-    <a href="https://github.com/matthewscharles/"><img src="icons/github-mark.svg" height="15em"> matthewscharles</a>
+    
+    
     <div class="card links">
       ${links}
     </div>
-    <hr>
+    
 `
